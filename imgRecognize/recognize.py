27fc5e4cbab707ef4df6ff78d7a921c893498cea
rsimari <rsimari@nd.edu>
2016-11-30 11:38:54 -0500

    now using argv for input files

--- imgRecognize/recognize.py
+++ imgRecognize/recognize.py
@@ -1,6 +1,9 @@
 import cv2
 import numpy as np 
 from matplotlib import pyplot as plt 
+import sys
+# from pyimagesearch.shapedetector import ShapeDetector
+
 
 class CVision(object):
 	def __init__(self, image = None, template = None):
@@ -9,6 +12,7 @@ class CVision(object):
 		self.template = None
 		self.width = None
 		self.height = None
+		self.plot = False
 		self.set_image(image)
 		self.set_template(template)
 		# self.methods = ['cv2.TM_CCOEFF', 'cv2.TM_CCOEFF_NORMED', 'cv2.TM_CCORR', 'cv2.TM_CCORR_NORMED', 'cv2.TM_SQDIFF', 'cv2.TM_SQDIFF_NORMED']
@@ -29,15 +33,12 @@ class CVision(object):
 	def find_image(self, image = None):
 		if image is None:
 			self.set_image(image)
-		location = []
 		for m in self.methods:
 			# self.image = self.image2.copy()
-			tl, br = self.use_method(m, True)
+			tl, br = self.use_method(m)
 			print tl, br
-			location.append((tl, br))
-		# aggregate the location numbers
 
-	def use_method(self, meth, plot = False):
+	def use_method(self, meth):
 		method = eval(meth)
 		# Apply template Matching
 		res = cv2.matchTemplate(self.image, self.template, 4)
@@ -49,10 +50,10 @@ class CVision(object):
 		else:
 			top_left = max_loc
 		bottom_right = (top_left[0] + self.width, top_left[1] + self.height)
-
-		# show rectangle of where the target is 
-		cv2.rectangle(self.image, top_left, bottom_right, (0,200,50), 10)
-		if plot:
+		# plot result if self.plot is set to True
+		if self.plot:
+			# show rectangle of where the target is 
+			cv2.rectangle(self.image, top_left, bottom_right, (0,200,50), 10)
 			plt.subplot(121),plt.imshow(res,cmap = 'gray')
 			plt.title('Matching Result'), plt.xticks([]), plt.yticks([])
 			plt.subplot(122),plt.imshow(self.image,cmap = 'gray')
@@ -63,5 +64,6 @@ class CVision(object):
 		return top_left, bottom_right
 		
 if __name__ == "__main__":
-	vision = CVision('find.JPG', 'small.JPG')
+	vision = CVision(sys.argv[1], sys.argv[2])
+	# vision.plot = False
 	vision.find_image()
