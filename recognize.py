fba0663a4a1238ffe699b534b732fdda7fb0932e
rsimari <rsimari@nd.edu>
2016-11-30 10:45:12 -0500

    moved to object oriented cv

--- recognize.py
+++ recognize.py
@@ -2,37 +2,66 @@ import cv2
 import numpy as np 
 from matplotlib import pyplot as plt 
 
-img = cv2.imread('find.JPG', 0)
-img2 = img.copy()
-template = cv2.imread('small.JPG',0)
-w, h = template.shape[::-1]
-# adjust these for which ever one works best
-# methods = ['cv2.TM_CCOEFF', 'cv2.TM_CCOEFF_NORMED', 'cv2.TM_CCORR', 'cv2.TM_CCORR_NORMED', 'cv2.TM_SQDIFF', 'cv2.TM_SQDIFF_NORMED']
-methods = ['cv2.TM_CCOEFF']
+class CVision(object):
+	def __init__(self, image = None, template = None):
+		self.image = None
+		self.image2 = None
+		self.template = None
+		self.width = None
+		self.height = None
+		self.set_image(image)
+		self.set_template(template)
+		# self.methods = ['cv2.TM_CCOEFF', 'cv2.TM_CCOEFF_NORMED', 'cv2.TM_CCORR', 'cv2.TM_CCORR_NORMED', 'cv2.TM_SQDIFF', 'cv2.TM_SQDIFF_NORMED']
+		self.methods = ['cv2.TM_CCOEFF']
 
-for meth in methods:
-	img = img2.copy()
-	method = eval(meth)
+	def set_image(self, image):
+		# so the interface can change the images
+		if self.image is None:
+			self.image = cv2.imread(image,0)
+			self.image2 = self.image.copy()
 
-	# Apply template Matching
-	res = cv2.matchTemplate(img,template,method)
-	min_val, max_val, min_loc, max_loc = cv2.minMaxLoc(res)
+	def set_template(self, template):
+		# so the interface can change the template
+		if self.template is None:
+			self.template = cv2.imread(template,0)
+			self.width, self.height = self.template.shape[::-1]
 
-	# If the method is TM_SQDIFF or TM_SQDIFF_NORMED, take minimum
-	if method in [cv2.TM_SQDIFF, cv2.TM_SQDIFF_NORMED]:
-		top_left = min_loc
-	else:
-		top_left = max_loc
-	bottom_right = (top_left[0] + w, top_left[1] + h)
+	def find_image(self, image = None):
+		if image is None:
+			self.set_image(image)
+		location = []
+		for m in self.methods:
+			# self.image = self.image2.copy()
+			tl, br = self.use_method(m, True)
+			print tl, br
+			location.append((tl, br))
+		# aggregate the location numbers
 
-	# show rectangle of where the target is 
-	# cv2.rectangle(img,top_left, bottom_right, 255, 2)
-	cv2.rectangle(img,top_left,bottom_right,(0,200,50),10)
+	def use_method(self, meth, plot = False):
+		method = eval(meth)
+		# Apply template Matching
+		res = cv2.matchTemplate(self.image, self.template, 4)
+		min_val, max_val, min_loc, max_loc = cv2.minMaxLoc(res)
 
-	plt.subplot(121),plt.imshow(res,cmap = 'gray')
-	plt.title('Matching Result'), plt.xticks([]), plt.yticks([])
-	plt.subplot(122),plt.imshow(img,cmap = 'gray')
-	plt.title('Detected Point'), plt.xticks([]), plt.yticks([])
-	plt.suptitle(meth)
-	plt.show()
-	
+		# If the method is TM_SQDIFF or TM_SQDIFF_NORMED, take minimum
+		if method in [cv2.TM_SQDIFF, cv2.TM_SQDIFF_NORMED]:
+			top_left = min_loc
+		else:
+			top_left = max_loc
+		bottom_right = (top_left[0] + self.width, top_left[1] + self.height)
+
+		# show rectangle of where the target is 
+		cv2.rectangle(self.image, top_left, bottom_right, (0,200,50), 10)
+		if plot:
+			plt.subplot(121),plt.imshow(res,cmap = 'gray')
+			plt.title('Matching Result'), plt.xticks([]), plt.yticks([])
+			plt.subplot(122),plt.imshow(self.image,cmap = 'gray')
+			plt.title('Detected Point'), plt.xticks([]), plt.yticks([])
+			plt.suptitle(meth)
+			plt.show()
+
+		return top_left, bottom_right
+		
+if __name__ == "__main__":
+	vision = CVision('find.JPG', 'small.JPG')
+	vision.find_image()
