18656cb30c146464ff4612b78dfae00fb5ceeac6
rsimari <rsimari@nd.edu>
2016-12-03 14:13:35 -0500

    more get_coordinates, moved functions into CVision

--- imgRecognize/CVision.py
+++ imgRecognize/CVision.py
@@ -10,6 +10,7 @@ import numpy as np
 from matplotlib import pyplot as plt 
 import sys
 import os
+import math
 import imutils
 
 # Example use: python CVision.py small.jpg find.JPG
@@ -100,14 +101,55 @@ class CVision(object):
 			return -1,-1
 		return self.image.shape
 
+	def get_coordinates(self, height, current_location):
+		# vision = CVision('photos/target.JPG', 'photos/capture.png')
+		pic_height, pic_width = self.get_image_size()
+		self.plot = True
+		x,y = self.find_image()
+		# now convert from m to degrees
+		dw = self.get_dwidth(height, pic_width, x[0]) / 1.113195e5
+		dh = self.get_dheight(height, pic_height, x[1]) / 1.113195e5
+		return current_location.latitude + dw, current_location.longitude + dh
+
+	def get_dwidth(self, height, pic_width, x):
+		# uses FOV angle to find the real life distance of half the picture
+		opp = math.tan(118.2 * 2 * math.pi) * height 
+		# ratio of real life to picture
+		ratio = 2 * opp / pic_width 
+		# center of picture in pixels
+		center = pic_width / 2
+		# difference between found spot and center of picture
+		dw = x - center
+		# returns the actual horizontal distance from found target
+		return dw * ratio
+
+	def get_dheight(self, height, pic_height, y):
+		# uses FOV angle to find the real life distance of half the picture
+		opp = math.tan(69.5 * 2 * math.pi) * height 
+		# ratio of real life to picture
+		ratio = 2 * opp / pic_height
+		# center of picture in pixels
+		center = pic_height / 2
+		# difference between found spot and center of picture
+		dh = y - center
+		# returns the actual vertical distance from found target
+		return dh * ratio
+
+class location(object):
+	def __init__(self, lat, lon):
+		self.latitude = lat
+		self.longitude = lon
+
 if __name__ == "__main__":
 	if len(sys.argv) < 3:
 		print "usage: python CVision.py template target"
 		exit(1)
 	vision = CVision(sys.argv[1], sys.argv[2])
-	i = vision.prep_image()
-	vision.save_image(i, 'out.png')
-	vision.plot = True
-	x,y = vision.find_image('out.png')
-	print x,y
+	# i = vision.prep_image()
+	# vision.save_image(i, 'out.png')
+	# vision.plot = True
+	# x,y = vision.find_image('out.png')
+	# print x,y
+	loc = location(4.12, 43.8)
+	print vision.get_coordinates(10, loc)
 
