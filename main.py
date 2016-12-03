ab9c232081231d61d2680e3ab97bb2a4397c09b4
rsimari <rsimari@nd.edu>
2016-12-03 15:04:25 -0500

    corrected the no found imagelogic

--- main.py
+++ main.py
@@ -13,7 +13,6 @@ class location(object): # temp class, simulating the dronekit location one
 loc = location(4.12, 43.8)
 
 vision = CVision('./photos/orange.png', './photos/blue.jpg')
-# TODO: what to do if none found?
 lat, lon = vision.get_coordinates(10, loc)
 print lat, lon
 # drone fly to lat, lon
