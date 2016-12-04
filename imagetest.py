a9556c3bf2d4fc3afd4a419c469334fe1d4d1f62
rsimari <rsimari@nd.edu>
2016-12-04 14:57:07 -0500

    added rm for old photo

--- imagetest.py
+++ imagetest.py
@@ -4,6 +4,7 @@ from dronekit import connect,VehicleMode, LocationGlobalRelative, time
 import os
 
 go = GoPro()
+os.remove('./localPicture.jpg')
 go.take_picture()
 # Dowloads into the file "localPicture.jpg"
 go.download_photo()
@@ -11,6 +12,7 @@ go.download_photo()
 # Pass Photo To Image Recognition
 #######################################################################$
 if not os.path.isfile('./localPicture.jpg'):
+	print "Error: image not downloaded correctly"
 	exit(1)
 vision = CVision('./localPicture.jpg', './imgRecognize/target.JPG')
 test = LocationGlobalRelative(41.000,42.000,0)
