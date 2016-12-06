50d2aa0c3b936ef4313124d5345050a815f2e831
rsimari <rsimari@nd.edu>
2016-12-05 20:21:35 -0500

    updated unit tests

--- test.py
+++ test.py
@@ -11,34 +11,37 @@ class VisionTest(unittest.TestCase):
 		self.assertEqual(vision.image, None)
 		self.assertEqual(vision.template, None)
 
-		vision = CVision('./imgRecognize/orange.png', './imgRecognize/find.JPG')
-		self.assertEqual(vision.image_str, './imgRecognize/orange.png')
-		self.assertEqual(vision.template_str, './imgRecognize/find.JPG')
+		vision = CVision('photos/orange.png', 'photos/find.JPG')
+		self.assertEqual(vision.image_str, 'photos/orange.png')
+		self.assertEqual(vision.template_str, 'photos/find.JPG')
 
 	def test_set_image(self):
 		vision = CVision()
-		vision.set_image('imgRecognize/orange2.jpg', gray = True)
-		self.assertEqual(vision.image.all(), cv2.imread('imgRecognize/orange2.jpg', 0).all())
+		vision.set_image('photos/orange2.jpg', gray = True)
+		self.assertEqual(vision.image.all(), cv2.imread('photos/orange2.jpg', 0).all())
 
-		vision = CVision('./imgRecognize/orange.png', './imgRecognize/find.JPG')
-		vision.set_image('imgRecognize/small.jpg', gray = True)
-		self.assertEqual(vision.image.all(), cv2.imread('imgRecognize/small.jpg', 0).all())
+		vision = CVision('photos/orange.png', 'photos/find.JPG')
+		vision.set_image('photos/small.jpg', gray = True)
+		self.assertEqual(vision.image.all(), cv2.imread('photos/small.jpg', 0).all())
 
 	def test_set_template(self):
 		vision = CVision()
-		vision.set_template('imgRecognize/orange2.jpg', gray = True)
-		self.assertEqual(vision.template.all(), cv2.imread('imgRecognize/orange2.jpg', 0).all())
+		vision.set_template('photos/orange2.jpg', gray = True)
+		self.assertEqual(vision.template.all(), cv2.imread('photos/orange2.jpg', 0).all())
 
-		vision = CVision('./imgRecognize/orange.png', './imgRecognize/find.JPG')
-		vision.set_template('imgRecognize/small.jpg', gray = True)
-		self.assertEqual(vision.template.all(), cv2.imread('imgRecognize/small.jpg', 0).all())
+		vision = CVision('photos/orange.png', 'photos/find.JPG')
+		vision.set_template('photos/small.jpg', gray = True)
+		self.assertEqual(vision.template.all(), cv2.imread('photos/small.jpg', 0).all())
 
 	def test_find_image(self):
-		vision = CVision('imgRecognize/small.jpg', './imgRecognize/find.JPG')
+		vision = CVision('photos/small.jpg', 'photos/find.JPG')
 		x,y = vision.find_image()
 		self.assertIsNotNone(x)
 		self.assertIsNotNone(y)
 
+	def test_get_coordinates(self):
+		pass
+		# TODO: write this test
 
 if __name__ == "__main__":
 	unittest.main()
